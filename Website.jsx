412bb149f091c59e0bad0f2567d048242055da53
Gaetano Izzo <gaetanoizzo2001@gmail.com>
2023-06-07 16:18:59 +0200

    Prima schermata

---
+++ Website.jsx
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap';
+
+export default function Website() {
+  return (
+    <>
+    <Container fluid>
+      <h1>Website</h1>
+    </Container>
+    </>
+  );
+}
