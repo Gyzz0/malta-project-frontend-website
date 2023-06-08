c23c053c2df59e6984b7aa8c4557b85219abd0fe
Gaetano Izzo <gaetanoizzo2001@gmail.com>
2023-06-08 18:40:40 +0200

    Modifiche di stile

--- home/WelcomeXL.jsx
+++ home/WelcomeXL.jsx
@@ -7,7 +7,7 @@ export default function WelcomeXL() {
     <Container fluid>
       <Row>
         <Col xl={{span:6, offset:1}}><img width={920} height={460}/></Col>
-        <Col xl={{offset:1}}><UserForms /></Col>
+        <Col xl={{offset:1, span:3}}><UserForms /></Col>
       </Row>
       <Row className="mt-2">
         <Col xl={{offset:1}}>Messaggio di benvenuto e descrizione della landa</Col>
