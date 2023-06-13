33c14840205d36f332fdd192280ea606ad540456
Gaetano Izzo <gaetanoizzo2001@gmail.com>
2023-06-13 14:15:30 +0200

    Lettura del IdGiocatore tramite il token

--- landa/LandaXL.jsx
+++ landa/LandaXL.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import OffXL from "./OFFXL";
+import { authPost } from "../../../assets/js/auth";
 
 
 export default function LandaXL() {
   const [schermata, setSchermata] = useState("OFF");
+  const [idGiocatore, setIdGiocatore] = useState(null);
+
+  useEffect(async() => {
+    setIdGiocatore(JSON.parse(await authPost("getIdGiocatoreByToken")))
+  
+  }, [])
+  
 
   return (
     <Container fluid>
@@ -12,7 +20,7 @@ export default function LandaXL() {
         {
           schermata == "ON"
             ? null //ON
-            : <OffXL />
+            : <OffXL idGiocatore={idGiocatore}/>
         }
       </Row>
     </Container>
